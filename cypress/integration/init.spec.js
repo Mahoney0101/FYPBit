82bea8a59c8924f37f1e4afaecb63a45d46764d8
Mahoney0101 <hoooch.mahoney@gmail.com>
2021-02-09 18:44:34 +0000

    Sonarcloud Test

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -2,7 +2,6 @@ describe('Cypress', () => {
     it('is working', () => {     
         expect(true).to.equal(true)   
     }) 
-    
     it('opens the app', () => {   
         cy.visit('https://master.d3izcz1kjo83i.amplifyapp.com/') 
     })
