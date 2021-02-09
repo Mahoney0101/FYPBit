38cfe1b8f5472d94ad517ea3c194884f9d8378dd
Mahoney0101 <hoooch.mahoney@gmail.com>
2021-02-09 16:49:02 +0000

    Added cyrus for CI testing added cyprus working test

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -1,9 +1,5 @@
-describe('Cypress', () => {   
-    it('is working', () => {     
-        expect(true).to.equal(true)   
-    }) 
-    
-    it('opens the app', () => {   
-        cy.visit('https://master.d3izcz1kjo83i.amplifyapp.com/') 
+describe('Cypress', () => {
+    it('is working', () => {
+      expect(true).to.equal(true)
     })
-})
+  })
