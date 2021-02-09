332817c77a8907d9a9f1a35d9d76c0746ec25c51
Mahoney0101 <hoooch.mahoney@gmail.com>
2021-02-09 17:46:19 +0000

    App URL test

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -1,5 +1,9 @@
-describe('Cypress', () => {
-    it('is working', () => {
-      expect(true).to.equal(true)
+describe('Cypress', () => {   
+    it('is working', () => {     
+        expect(true).to.equal(true)   
+    }) 
+    
+    it('opens the app', () => {   
+        cy.visit('https://master.d3izcz1kjo83i.amplifyapp.com/') 
     })
-  })
+})
